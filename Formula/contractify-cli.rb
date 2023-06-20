c6bd9693e77f86b13d2b5ad721a912fc7c26a693
Pieter Claerhout <pieter@contractify.be>
2023-06-20 12:18:23 +0000

    Bumped to version 1.21.0

--- Formula/contractify-cli.rb
+++ Formula/contractify-cli.rb
@@ -1,9 +1,9 @@
 class ContractifyCli < Formula
   desc "CLI interface for Contractify"
   homepage "https://contractify.io"
-  url "https://contractify-builds.fra1.cdn.digitaloceanspaces.com/contractify-cli/e5a8c0db059c5776bc9b1e1e3d514189bce672f6/contractify-cli-macos.tar.gz"
-  version "1.20.0"
-  sha256 "f3dc44d570785ec9d4a9718994011199dbf0e49c4b3062ed24a2e6a40408ce70"
+  url "https://contractify-builds.fra1.cdn.digitaloceanspaces.com/contractify-cli/e3309f5a3af7207404b693b602405f1a752e7a6f/contractify-cli-macos.tar.gz"
+  version "1.21.0"
+  sha256 "803f49f2c3d45e0f43836d2ff01f4833bee79b323a1d6f2fa1a3defb1abfc26c"
   license "MIT"
 
   def install
